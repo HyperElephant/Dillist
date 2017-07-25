c62da3552dfb72523988aa23ac569c90497e7fdf
David Taylor <dktaylor1701@gmail.com>
2017-07-24 21:07:09 -0400

    Added route to get current user's wishes. Started functionality to delete a wish by id.

--- models/Wish.js
+++ models/Wish.js
@@ -1,16 +1,18 @@
 var mongoose = require('mongoose');
 var uniqueValidator = require('mongoose-unique-validator');
+var User = mongoose.model('User');
 
 var WishSchema = new mongoose.Schema({
   title: String,
   url: String,
   author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
-});
+}, {timestamps: true});
 
 WishSchema.plugin(uniqueValidator, {message: 'is already taken'});
 
 WishSchema.methods.toJSONFor = function(user) {
   return{
+    id: this._id,
     title: this.title,
     url: this.url,
     author: this.author.toProfileJSONFor(user)
--- routes/api/wishes.js
+++ routes/api/wishes.js
@@ -5,6 +5,16 @@ var Wish = mongoose.model('Wish');
 var User = mongoose.model('User');
 var auth = require('../auth');
 
+router.param('wish', function(req, res, next, id) {
+  Wish.findById(id).then(function(wish){
+    if(!wish) { return res.sendStatus(404); }
+
+    req.wish = wish;
+
+    return next();
+  }).catch(next);
+});
+
 router.post('/wishes', auth.required, function(req, res, next){
   User.findById(req.payload.id).then(function(user){
     if (!user) { return res.sendStatus(401); }
@@ -20,5 +30,50 @@ router.post('/wishes', auth.required, function(req, res, next){
   }).catch(next);
 })
 
+router.get('/wishes', auth.required, function(req, res, next) {
+  var limit = 20;
+  var offset = 0;
+
+  if(typeof req.query.limit !== 'undefined'){
+    limit = req.query.limit;
+  }
+
+  if(typeof req.query.offset !== 'undefined'){
+    offset = req.query.offset;
+  }
+
+  User.findById(req.payload.id).then(function(user){
+    if (!user) { return res.sendStatus(401); }
+
+    Promise.all([
+      Wish.find({ author: user})
+        .limit(Number(limit))
+        .skip(Number(offset))
+        .populate('author')
+        .exec(),
+      Wish.count({ author: user.following})
+    ]).then(function(results){
+      var wishes = results[0];
+      var wishesCount = results[1];
+
+      return res.json({
+        wishes: wishes.map(function(wish){
+          return wish.toJSONFor(user);
+        }),
+        wishesCount: wishesCount
+      });
+    }).catch(next);
+  });
+});
+
+router.delete('/wishes/:wish', auth.required, function(req, res, next) {
+  if(req.wish.author._id.toString() === req.payload.id.toString()){
+    return req.wish.remove().then(function(){
+      return res.sendStatus(204);
+    });
+  } else {
+    return res.sendStatus(403);
+  }
+});
 
 module.exports = router;
